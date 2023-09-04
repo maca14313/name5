7f221256b0f6f8045453ce61a5bf5f25d4f9b899
muhammad <muhammad@muhammads-MacBook-Pro.local>
2023-09-04 17:42:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Counter } from './features/counter/Counter';
 import './app.css';
-import Reg from './compenent/reg/Reg'
-import PhoneNumberList from './compenent/reg/PhoneNumberList';
-import MemberProfile from './compenent/reg/MemberProfile';
-import ListOfMembers from './compenent/reg/ListOfMembers';
-import ListOfPhoneNumber from './compenent/reg/ListOfPhoneNumber';
-import UpdateMember from './compenent/reg/UpdateMember';
-import AdminUpdateMember from './compenent/reg/AdminUpdateMember';
-import HomePage from './compenent/reg/HomePage';
+
 
 function App() {
   //        <Route path="/driverhome/:name/:id" element={<DriverHome/>}/>
   return (
-    <Router>
 
     <div className="App">
   
-      <Routes>
-        
-        <Route path="/registretion" element={<Reg/>}/>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/add-phone-number" element={<PhoneNumberList/>}/>
-        <Route path="/memberprofile" element={<MemberProfile/>}/>
-        <Route path="/listofmembers" element={<ListOfMembers/>}/>
-        <Route path="/listofphonenumber" element={<ListOfPhoneNumber/>}/>
-        <Route path="/updatemember/:phone_number" element={<UpdateMember/>}/>
-        <Route path="/adminupdatemember/:phone_number" element={<AdminUpdateMember/>}/>
-
-
-
-
-
-
-
-
-     </Routes>
-
+     hello
     </div>
     
-    </Router>
   );
 }
 
